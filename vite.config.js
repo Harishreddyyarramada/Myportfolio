e6dc38c6b5aa39dbd0126074d93c4423ee8f0382
Hari112005 <yarramadaharishreddy@gmail.com>
2025-09-17 22:46:06 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://yarramadaharishreddy.vercel.app',
+    }),
+  ],
 });
